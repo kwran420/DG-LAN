98b810801ae2482e4fb04da4c4e5b24d8daeedf7
Kieran Hollis <kwran420@github>
2026-04-05 19:47:41 +0930

    chore: bump version to 1.2.2 Alpha

--- application/Common/Version.h
+++ application/Common/Version.h
@@ -18,9 +18,9 @@
 
 #pragma once
 
-#define VERSION "1.2.1"
+#define VERSION "1.2.2"
 #define VERSION_TAG "Alpha"
 
 // These two values are automatically updated during the release building process. See the script 'Application/Tools/update_version.sh'
-#define BUILD_TIME "2026-04-05_18-00"
-#define GIT_VERSION "c269db36533bca3137e7b440693b06bc9a4c6838"
+#define BUILD_TIME "2026-04-05_22-00"
+#define GIT_VERSION "49eb691d239ed0acc26587263ba9e51778b65a75"
