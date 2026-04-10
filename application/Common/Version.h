6bedddf5c63ce0661541f15d59f8e7362d896472
Kieran Hollis <kwran420@github>
2026-04-10 14:36:42 +0930

    chore: release v1.2.52

--- application/Common/Version.h
+++ application/Common/Version.h
@@ -18,9 +18,9 @@
 
 #pragma once
 
-#define VERSION "1.2.51"
+#define VERSION "1.2.52"
 #define VERSION_TAG "Alpha"
 
 // These two values are automatically updated during the release building process. See the script 'Application/Tools/update_version.sh'
-#define BUILD_TIME "2026-04-10_04-55"
-#define GIT_VERSION "01367def99f0"
+#define BUILD_TIME "2026-04-10_05-05"
+#define GIT_VERSION "2014ec5cc150"
